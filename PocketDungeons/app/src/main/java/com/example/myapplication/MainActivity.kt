57367a1fac3f8b54cd149e466fe7a4ec543bfedb
MiguelAMartinezP <73351534+MiguelAMartinezP@users.noreply.github.com>
2025-03-07 13:30:59 +0100

    Add files via upload

--- PocketDungeons/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ PocketDungeons/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -7,11 +7,21 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import android.widget.Toast
 import android.app.Activity
+import android.content.Intent
+import android.widget.Button
 
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var playButton: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        playButton = findViewById(R.id.PlayButton)
+
+        playButton.setOnClickListener{
+            val intent = Intent(this, PlayMenu::class.java)
+            startActivity(intent)
+        }
     }
 }
