2ca45630f7c9dc57e5d1f3642a976f7c71503305
MiguelAMartinezP <bynixi334@gmail.com>
2025-03-20 19:46:22 +0100

    proyecto bien subido
    y encima con los botones con imágenes

---
+++ PocketDungeons/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import android.widget.Toast
+import android.app.Activity
+import android.content.Intent
+import android.widget.Button
+import android.widget.ImageView
+
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var playButton: ImageView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        playButton = findViewById(R.id.PlayButton)
+
+        playButton.setOnClickListener{
+            val intent = Intent(this, PlayMenu::class.java)
+            startActivity(intent)
+        }
+    }
+}
